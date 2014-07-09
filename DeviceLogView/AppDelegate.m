c6802d47c8724b195a7a4e28abe19552fa6f54dd
JongYoon.Ahn <gmggae@gmail.com>
2014-07-09 19:19:56 +0900

    remove unnecessary function and comment

--- DeviceLogView/AppDelegate.m
+++ DeviceLogView/AppDelegate.m
@@ -28,42 +28,11 @@ static CFMutableDictionaryRef liveConnections;
 static int debug;
 static CFStringRef requiredDeviceId;
 static char requiredProcessName[256];
-//static void (*printMessage)(int fd, const char *, size_t);
 static void DeviceNotificationCallback(am_device_notification_callback_info *info, void *unknown);
 AppDelegate *sampleClass;
 
-
-/*
-
-- (void)tableView:(NSTableView *)tableView heightOfRow:(NSInteger)row logString:(NSString*)content
-{
-    NSTableColumn *col = [[_logTable tableColumns] objectAtIndex:3];
-    float colWidth = [col width];
-    
-    //NSString *content = [[[_arrayController arrangedObjects]    objectAtIndex:row] valueForKey:@"context"];
-    
-    float textWidth = [content sizeWithAttributes:[NSDictionary
-                                                   dictionaryWithObject:[NSFont fontWithName:@"LucidaGrande" size:13.00]
-                                                   forKey:@"NSFontAttributeName"]].width;
-    
-    float newHeight = ceil(textWidth/colWidth);
-    
-    // Checking to see the sizes of everything involved
-    //NSLog(@"content: %@ textWidth: %f, colWidth: %f, newHeight: %f", content, textWidth,     colWidth, newHeight);
-    
-    newHeight = newHeight * 17.0;
-    
-    //[tableView noteHeightOfRowsWithIndexesChanged:[NSIndexSet indexSetWithIndexesInRange:NSMakeRange(row, newHeight)]];
-
-}
- */
-
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
-    // Insert code here to initialize your application
-    //printMessage = &write_fully;
-    //printSeparatorprintSeparator = &plain_separator ;
-    //[self addRowData:@"1" :@"2" :@"3" :@"4"];
     
     sampleClass = self;
     
@@ -84,24 +53,17 @@ AppDelegate *sampleClass;
 
 
 - (void)addRowData:(NSString*)device :(NSString*)process :(NSString*)status :(NSString*)log :(NSColor*) color;{
-    /*[self.LogTextView setString:[NSString stringWithFormat:@"%@\n%@",[self.LogTextView string],text]];
-    [self.LogTextView scrollRangeToVisible:NSMakeRange([[self.LogTextView string] length], 0)];*/
-    
-    
     
     [_arrayController addObject:[NSDictionary dictionaryWithObjectsAndKeys:
                                  device,@"device",
                                  process,@"process",
                                  status,@"status",
-                                 log,@"log", nil]];
+                                 log,@"log",
+                                 color,@"deviceColor",
+                                 color,@"processColor",
+                                 color,@"statusColor",
+                                 color,@"logColor",nil]];
     
-   
-    
-    //_logTable.rowHeight = [self tableView:_logTable heightOfRow: [_logTable rowHeight]];
-    
-    
-    
-   // [_arrayController set]
     
     NSInteger numberOfRows = [_logTable numberOfRows];
     
@@ -118,34 +80,12 @@ AppDelegate *sampleClass;
 
 - (IBAction)ClearButton:(NSButton*)sender
 {
-    //[self.LogTextView setString:@""];
     [[self.arrayController mutableArrayValueForKey:@"content"] removeAllObjects];
 }
 
 
 
 @end
-
-
-
-/*
-static inline void write_fully(int fd, const char *buffer, size_t length)
-{
-    while (length) {
-        ssize_t result = write(fd, buffer, length);
-        if (result == -1)
-            break;
-        buffer += result;
-        length -= result;
-    }
-}*/
-
-/*
-static inline void write_string(int fd, const char *string)
-{
-    write_fully(fd, string, strlen(string));
-}
- */
 
 static int find_space_offsets(const char *buffer, size_t length, size_t *space_offsets_out)
 {
@@ -221,13 +161,9 @@ static void write_colored(int fd, const char *buffer, size_t length)
         
         
         // date and device name
-        //write_const(fd, COLOR_DARK_WHITE);
-       // write_fully(fd, buffer, space_offsets[0]);
         device = [[NSString alloc] initWithBytes:buffer
                                        length:space_offsets[0] encoding:NSUTF8StringEncoding];
 
-        
-        
         
         // process
         int pos = 0;
@@ -238,13 +174,7 @@ static void write_colored(int fd, const char *buffer, size_t length)
             }
         }
         
-        //write_const(fd, COLOR_CYAN);
-        
         if (pos && buffer[space_offsets[1]-1] == ']') {
-            //write_fully(fd, buffer + space_offsets[0], pos - space_offsets[0]);
-            //write_const(fd, COLOR_DARK_CYAN);
-            //write_fully(fd, buffer + pos, space_offsets[1] - pos);
-            
             NSString* p1 = [[NSString alloc] initWithBytes:buffer+space_offsets[0]
                                                length:pos-space_offsets[0] encoding:NSUTF8StringEncoding];
             NSString* p2 = [[NSString alloc] initWithBytes:buffer+pos
@@ -255,8 +185,6 @@ static void write_colored(int fd, const char *buffer, size_t length)
             
             
         } else {
-            //write_fully(fd, buffer + space_offsets[0], space_offsets[1] - space_offsets[0]);
-            
             process = [[NSString alloc] initWithBytes:buffer+space_offsets[0]
                                                     length:space_offsets[1]-space_offsets[0] encoding:NSUTF8StringEncoding];
         }
@@ -267,45 +195,24 @@ static void write_colored(int fd, const char *buffer, size_t length)
         // Log level
         size_t levelLength = space_offsets[2] - space_offsets[1];
         if (levelLength > 4) {
-            //const char *normalColor;
-           // const char *darkColor;
             if (levelLength == 9 && memcmp(buffer + space_offsets[1], " <Debug>:", 9) == 0){
-                //normalColor = COLOR_MAGENTA;
-                //darkColor = COLOR_DARK_MAGENTA;
                 color = [NSColor blueColor];
             } else if (levelLength == 11 && memcmp(buffer + space_offsets[1], " <Warning>:", 11) == 0){
-                //normalColor = COLOR_YELLOW;
-                //darkColor = COLOR_DARK_YELLOW;
                 color = [NSColor yellowColor];
             } else if (levelLength == 9 && memcmp(buffer + space_offsets[1], " <Error>:", 9) == 0){
-                //normalColor = COLOR_RED;
-                //darkColor = COLOR_DARK_RED;
                 color = [NSColor redColor];
             } else if (levelLength == 10 && memcmp(buffer + space_offsets[1], " <Notice>:", 10) == 0) {
-                //normalColor = COLOR_GREEN;
-                //darkColor = COLOR_DARK_GREEN;
                 color = [NSColor greenColor];
             } else {
                 goto level_unformatted;
             }
-            //write_string(fd, darkColor);
-            //write_fully(fd, buffer + space_offsets[1], 2);
             NSString* s1 = [[NSString alloc] initWithBytes:buffer+space_offsets[1]
                                                     length:2 encoding:NSUTF8StringEncoding];
-            
-            //write_string(fd, normalColor);
-            //write_fully(fd, buffer + space_offsets[1] + 2, levelLength - 4);
             NSString* s2 = [[NSString alloc] initWithBytes:buffer+space_offsets[1]+2
                                                     length:levelLength-4 encoding:NSUTF8StringEncoding];
 
-            
-            //write_string(fd, darkColor);
-            //write_fully(fd, buffer + space_offsets[1] + levelLength - 2, 1);
             NSString* s3 = [[NSString alloc] initWithBytes:buffer+space_offsets[1]+levelLength-2
                                                     length:1 encoding:NSUTF8StringEncoding];
-
-            //write_const(fd, COLOR_DARK_WHITE);
-            //write_fully(fd, buffer + space_offsets[1] + levelLength - 1, 1);
             NSString* s4 = [[NSString alloc] initWithBytes:buffer+space_offsets[1]+levelLength-1
                                                     length:1 encoding:NSUTF8StringEncoding];
             
@@ -313,25 +220,16 @@ static void write_colored(int fd, const char *buffer, size_t length)
             
 
         } else {
+
         level_unformatted:
-            //write_const(fd, COLOR_RESET);
-            //write_fully(fd, buffer + space_offsets[1], levelLength);
             status = [[NSString alloc] initWithBytes:buffer+space_offsets[1]
                                            length:levelLength encoding:NSUTF8StringEncoding];
-
         }
-        //write_const(fd, COLOR_RESET);
-        
-        
-        
         
         //Log
-        //write_fully(fd, buffer + space_offsets[2], length - space_offsets[2]);
         log = [[NSString alloc] initWithBytes:buffer+space_offsets[2]
                                        length:length-space_offsets[2] encoding:NSUTF8StringEncoding];
-
     } else {
-        //write_fully(fd, buffer, length);
         log = [[NSString alloc] initWithBytes:buffer
                                        length:length encoding:NSUTF8StringEncoding];
 
@@ -362,15 +260,8 @@ static void SocketCallback(CFSocketRef s, CFSocketCallBackType type, CFDataRef a
         }
         
         if (should_print_message(buffer, extentLength)) {
-           // NSString *marketPacket = [NSString stringWithCString:buffer encoding:NSASCIIStringEncoding];
-            //[sampleClass appendToMyTextView:marketPacket];
-           
             write_colored(1, buffer, extentLength);
-            
-            
-            
-            //printSeparator(1);
-        }
+         }
         
         length -= extentLength;
         buffer += extentLength;
@@ -449,19 +340,3 @@ static void DeviceNotificationCallback(am_device_notification_callback_info *inf
             break;
     }
 }
-
-/*static void no_separator(int fd)
- {
- }*/
-
-/*static void plain_separator(int fd)
-{
-    //write_const(fd, "--\n");
-}
-*/
-/*static void color_separator(int fd)
- {
- write_const(fd, COLOR_DARK_WHITE "--" COLOR_RESET "\n");
- }*/
-
-
