2735e74d3a157661d3c7d14ca002d5bee945568f
JongYoon.Ahn <gmggae@gmail.com>
2014-07-11 17:03:09 +0900

    (20140711) ILogFilter

---
+++ DeviceLogViewer/iLogFilterGUI.h
@@ -0,0 +1,13 @@
+//
+//  iLogFilterGUI.h
+//  DeviceLogViewer
+//
+//  Created by LINEPLUS on 2014. 7. 11..
+//  Copyright (c) 2014년 line. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface iLogFilterGUI : NSObject
+
+@end
---
+++ DeviceLogViewer/iLogFilterGUI.m
@@ -0,0 +1,13 @@
+//
+//  iLogFilterGUI.m
+//  DeviceLogViewer
+//
+//  Created by LINEPLUS on 2014. 7. 11..
+//  Copyright (c) 2014년 line. All rights reserved.
+//
+
+#import "iLogFilterGUI.h"
+
+@implementation iLogFilterGUI
+
+@end
